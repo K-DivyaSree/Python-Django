0209b1d1cc580e2922b490e953810697bcabf1db
K-DivyaSree <214g1a0523@srit.ac.in>
2024-08-24 22:27:36 +0530

    commit

--- App1/urls.py
+++ App1/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from App1 import views
 urlpatterns=[
-    path('wish',views.fun3)
+    path('',views.students,name='home')
 ]
--- App1/views.py
+++ App1/views.py
@@ -11,3 +11,13 @@ def fun3(request):
     a=int(request.GET.get('n1'))
     b=int(request.GET.get('n2'))
     return HttpResponse(f'The sum of 2 nums is {a+b}')
+
+# def home(request):
+#     d={'name':'divya'}
+#     return render(request,'../Templates/index.html',d)
+
+def students(request):
+    listofstudents=[['divya','CSE',523],['maya','CSE',552],['Navya','EEE',322]]
+    context={'data':listofstudents}
+    return render(request,'../Templates/home.html',context)
+    
